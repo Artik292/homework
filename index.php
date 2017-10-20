0ecdc12ea5e8ce429d7450198c88cdd755829ebe
Artur Konevnikov <artik292@gmail.com>
2017-10-20 20:51:59 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,11 +5,31 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Нажмите на любую кнопку!');
 $app->initLayout('Centered');
 
-$button1 = $app->layout->add(['Button', '1']);
-$button1->link(['main','number'=>1]);
+If (isset($_GET['max'])) {
+  $max = $_GET['max'];
+  $min = $_GET['min'];
+} else {
+  $max = 100;
+  $min = 0;
+}
 
-$button2 = $app->layout->add(['Button', '2']);
-$button2->link(['main','number'=>2]);
+$med = round(($max+$min)/2);
 
-$button3 = $app->layout->add(['Button', '3']);
-$button3->link(['main','number'=>3]);
+$label = $app->layout->add(['Label','Ваше число больше '.$med.' ?']);
+$label->addClass('massive orange');
+
+$button_yes = $app->layout->add(['Button','Да']);
+$button_yes->addClass('massive green');
+$button_yes->link(['index','max'=>$max,'min'=>$med]);
+
+$button_no = $app->layout->add(['Button','Нет']);
+$button_no->addClass('massive red');
+$button_no->link(['index','max'=>$med,'min'=>$min]);
+
+$button_win = $app->layout->add(['Button','Это моё число!']);
+$button_win->addClass('massive violet');
+$button_win->link(['win','number'=>$med]);
+
+$button_win = $app->layout->add(['Button','Сыграть ещё раз']);
+$button_win->addClass('small brown');
+$button_win->link(['index']);
